3e45bc40056a9b81cb4df3f58547d2e1c910c481
Jason Whited <jsn.whited@gmail.com>
2019-12-13 01:28:56 -0500

    fixing functions on index.js

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -54,7 +54,39 @@ panel.data("_id", article._id);
 return panel;
 }
 
+function renderEmpty =
+    $(["<div class='alert alert-warning text-center'>",
+        "<h4> there aint not articles!</h4>"
+    ].join(""));
+
+    articleContainer.append(emptyAlert);
 
 
+
+function handleArticleSave() {
+    var articleToSave = $(this).parents(".panel").data();
+    articleToSave.saved = true;
+
+    $.ajax({
+        method: "PATCH",
+        url: "/api/headlines",
+        data: articleToSave
+    })
+    .then(function(data){
+        if(data.ok) {
+            initPage();
+            }
+    });
+  }
+
+  function handleArticleScrape() {
+      $.get("/api/fetch")
+        .then(function(data) {
+
+            initPage();
+            bootbox.alert("<h3 class= 'text-center m-top-80'>" + data.message + "</h3>");
+
+        });
+  }
 
 })

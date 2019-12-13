bcdc8d8f86078b5fa3f40a2448fb24d9077fb18a
Jason Whited <jsn.whited@gmail.com>
2019-12-12 23:12:22 -0500

    more things

---
+++ controllers/headline.js
@@ -0,0 +1,16 @@
+var scrape = require("../scripts/scrape");
+var makeDate = require("../scripts/date");
+
+var Headline = require("../models/Headline");
+ 
+module.exports = {
+    fetch: function(cb) {
+        scrape(function(data) {
+            var articles = data;
+            for (var i=0; i <articles.length; i++) {
+                articles(i).date = makeDate();
+                articles(i).saved = false;
+            }
+        });
+    }
+}

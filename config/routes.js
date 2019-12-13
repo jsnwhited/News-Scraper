977fa6b1f3721d3dd307b1dffb016c20234a1041
Jason Whited <jsn.whited@gmail.com>
2019-12-13 00:27:15 -0500

    routers for notes

--- config/routes.js
+++ config/routes.js
@@ -45,6 +45,16 @@ module.exports = function(router)  {
             res.json(data);
         });
     });
+
+    router.patch("/api/headlines", function(req, res){
+        var query = {};
+        if (req.params.headline_id){
+            query._id = req.params.headline_id;
+        }
+        notesController.get(query, function(err, data){
+            res.json(data);
+        });
+    });
 }
 
  

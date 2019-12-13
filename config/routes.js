0e68aa9b7de3a9331948829620b78b20d7b39c8a
Jason Whited <jsn.whited@gmail.com>
2019-12-13 00:30:59 -0500

    router fix

--- config/routes.js
+++ config/routes.js
@@ -47,8 +47,14 @@ module.exports = function(router)  {
     });
 
     router.patch("/api/headlines", function(req, res){
+        headlinesController.update(req.body, function(err, data){
+            res.json(data);
+        });
+    });
+        
+    router.get("/api/notes/:headline_id?", function(req,res){
         var query = {};
-        if (req.params.headline_id){
+        if (req.params.headline_id) {
             query._id = req.params.headline_id;
         }
         notesController.get(query, function(err, data){
